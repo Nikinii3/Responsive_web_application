38e77206740d7b15b5bf7f1fdf94d2f6ed190401
Nikinii3 <nikzylive@gmail.com>
2026-07-09 23:49:49 +0530

    refactor(main): wrap app with BrowserRouter, DndProvider and FavouritesProvider

--- estate-agent/src/main.jsx
+++ estate-agent/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import App from './App.jsx';
+import { FavouritesProvider } from './context/FavouritesContext.jsx';
+import './styles/global.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <DndProvider backend={HTML5Backend}>
+        <FavouritesProvider>
+          <App />
+        </FavouritesProvider>
+      </DndProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
